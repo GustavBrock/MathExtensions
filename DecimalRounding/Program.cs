51bf195cd1b7267afb690ef2a60d77794c480903
GustavBrock <gustav@cactus.dk>
2015-07-28 20:33:25 +0200

    1

---
+++ DecimalRounding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test nr. 4.
+
+            // decimal inputValue = 31.675m;
+            //byte significantDigits = 4;
+
+        }
+    }
+}
---
+++ DecimalRounding/RoundSignificant.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DecimalExtensions
+{
+    public static class RoundingMethods
+    {
+        public static decimal RoundToSignificantDigits(this decimal inputValue, byte significantDigits)
+        {
+            bool integer = false;
+            return RoundingSignificant(inputValue, significantDigits, integer);
+        }
+
+        public static decimal RoundToSignificantDigits(this decimal inputValue, byte significantDigits, bool integer)
+        {
+            return RoundingSignificant(inputValue, significantDigits, integer);
+        }
+
+        private static decimal RoundingSignificant(decimal inputValue, byte significantDigits, bool integer)
+        {
+            const double base10 = 10;
+            double exponent = 0;
+            decimal scaling = 0;
+            decimal half = 0;
+            decimal scaledValue = 0;
+            decimal returnValue = 0;
+
+            if (significantDigits > 0 && inputValue != 0)
+            {
+                exponent = Math.Floor(Math.Log10((double)Math.Abs(inputValue))) + 1 - significantDigits;
+                if (integer == true && exponent < 0)
+                {
+                    // No decimals.
+                    exponent = 0;
+                }
+                scaling = (decimal)Math.Pow(base10, exponent);
+                scaledValue = inputValue / scaling;
+                half = Math.Sign(inputValue) / 2m;
+                returnValue = Math.Truncate(scaledValue + half) * scaling;
+            }
+            return returnValue;
+        }
+        
+    }
+}

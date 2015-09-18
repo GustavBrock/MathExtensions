c49baa86464d58416027c5c16954bd5cad985a19
Gustav Brock <gustav@cactus.dk>
2015-09-18 18:22:55 +0200

    FactorialSequence

--- DecimalRounding/Math.cs
+++ DecimalRounding/Math.cs
@@ -3,37 +3,118 @@
 
     public static class MathExt
     {
+        // Maximum value of Factorial number before overflow.
+        public const decimal FactorialMaxDecimal = 27m;
+        public const double FactorialMaxDouble = 170d;
+
         // Maximum length of Fibonacci sequences before overflow.
         public const int FibonacciMaxDecimal = 139;
         public const int FibonacciMaxDouble = 1476;
 
-        public static double Factorial(double value)
+        public static double Factorial(double number)
         {
-            // Maximum value for reliable result.
-            const double maxValue = 170;
+            // Result value for FactorialMaxDouble is:
+            //  7.25741561530799E+306
+            // Maximum count of significant figures for any value is 14.
+
             // Minimum result per definition.
             const double minResult = 1;
             // Error result for out-of-range values.
             const double errResult = 0;
 
-            if (value > maxValue)
+            double factorial = number;
+
+            if (number < 0 || number > FactorialMaxDouble)
             {
                 // Return value of error.
-                value = errResult;
+                factorial = errResult;
             }
             else
             {
-                if (value > minResult)
+                if (number > minResult)
                 {
                     // Calculate result by recursion.
-                    value *= Factorial(--value);
+                    factorial *= Factorial(--number);
                 }
                 else
                 {
-                    value = minResult;
+                    factorial = minResult;
                 }
             }
-            return value;
+            return factorial;
+        }
+
+        public static decimal Factorial(decimal number)
+        {
+            // Result value for FactorialMaxDecimal is:
+            //  10888869450418352160768000000
+
+            // Minimum result per definition.
+            const decimal minResult = 1m;
+            // Error result for out-of-range values.
+            const decimal errResult = 0m;
+
+            decimal factorial = number;
+
+            if (number < 0 || number > FactorialMaxDecimal)
+            {
+                // Return value of error.
+                factorial = errResult;
+            }
+            else
+            {
+                if (number > minResult)
+                {
+                    // Calculate result by recursion.
+                    factorial *= Factorial(--number);
+                }
+                else
+                {
+                    factorial = minResult;
+                }
+            }
+            return factorial;
+        }
+
+        public static decimal[] FactorialSequence(int values)
+        {
+            // Result value for FactorialMaxDecimal is:
+            //  10888869450418352160768000000
+
+            // Minimum result per definition.
+            const decimal minResult = 1m;
+            // Error result for out-of-range values.
+            const decimal errResult = 0m;
+
+            int numbers = values;
+
+            if (numbers < 0 || numbers > FactorialMaxDecimal)
+            {
+                numbers = 0;
+            }
+            // Always enable element zero.
+            int elements = 1 + numbers;
+
+            decimal[] factorialSequence = new decimal[elements];
+            factorialSequence[0] = minResult;
+
+            if (numbers > 0)
+            {
+                for (int i = 1; i < elements; i++)
+                {
+                    if (i == 1)
+                    // Fill element one per definition.
+                    {
+                        factorialSequence[i] = minResult;
+                    }
+                    else
+                    // Fill remaining elements by calculation.
+                    {
+                        factorialSequence[i] = numbers * factorialSequence[i - 1];
+                    }
+                }
+            }
+            return factorialSequence;
         }
 
         public static decimal Fibonacci(int value)

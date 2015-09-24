0e2f6a30e68f6f1f5b8c600af23cc23c966138b0
Gustav Brock <gustav@cactus.dk>
2015-09-24 09:30:59 +0200

    Common

---
+++ MathExt/Common.cs
@@ -0,0 +1,33 @@
+﻿namespace SystemExt
+{
+    #region Enums
+
+    public enum UpRounding
+    {
+        Up,
+        ToZero
+    }
+
+    public enum DownRounding
+    {
+        Down,
+        ToZero
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Provides constants and methods for rounding, generating mathematical sequences, and to retrieve single values from these.
+    /// </summary>
+    public static partial class MathExt
+    {
+        #region Constants
+
+        /// <summary>
+        /// Represents the base number for calculating scale when rounding to significant numbers.
+        /// </summary>
+        public const double Base10 = 10;
+
+        #endregion
+    }
+}

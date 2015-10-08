c9beabc573541e72524782921b8277f5d42c5fe0
Gustav Brock <gustav@cactus.dk>
2015-10-08 17:57:46 +0200

    Demo

--- MathExtDemo/Program.cs
+++ MathExtDemo/Program.cs
@@ -1,27 +1,39 @@
 ﻿using System;
 using SystemExt;
 
-
 namespace MathExtDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            decimal value = 30.675m;
-            int digits = 49;
+            Demo demo = new Demo();
+            demo.RoundingDemo();
+            demo.FactorialDemo();
+            demo.FibonacciDemo();
+        }
+    }
 
+    class Demo
+    { 
+        public void RoundingDemo()
+        {
+            decimal value = 30.675m;
+            int digits = 4;
+
+            Console.WriteLine("Scaling significant figures:");
             for (int i = 0; i < 10; i++)
             {
-                decimal roundedValue = MathExt.RoundSignificant(value, digits);
-                Console.WriteLine("input value: {0} - Rounded value: {1}", value.ToString(), roundedValue.ToString());
-                value++;
+                decimal testValue = value * (decimal)Math.Pow(10, i - 3);
+                decimal roundedValue = MathExt.RoundSignificant(testValue, digits);
+                Console.WriteLine("input value: {0} \t - Rounded value: {1}", ((double)testValue).ToString(), roundedValue.ToString());
             }
             Console.WriteLine();
 
             value = 30.675m;
             digits = 4;
 
+            Console.WriteLine("Rounding to even:");
             for (int i = 0; i < 10; i++)
             {
                 decimal roundedValue = MathExt.RoundSignificant(value, digits, MidpointRounding.ToEven);
@@ -29,10 +41,11 @@ namespace MathExtDemo
                 value++;
             }
             Console.WriteLine();
-            
+
             value = 30.685m;
             digits = 4;
 
+            Console.WriteLine("Rounding away from zero:");
             for (int i = 0; i < 10; i++)
             {
                 decimal roundedValue = MathExt.RoundSignificant(value, digits);
@@ -40,63 +53,71 @@ namespace MathExtDemo
                 value++;
             }
             Console.WriteLine();
-            
+
             value = 30.685m;
             digits = 4;
 
+            Console.WriteLine("Rounding to even:");
             for (int i = 0; i < 10; i++)
             {
                 decimal roundedValue = MathExt.RoundSignificant(value, digits, MidpointRounding.ToEven);
                 Console.WriteLine("input value: {0} - Rounded value: {1}", value.ToString(), roundedValue.ToString());
                 value++;
             }
+            Console.WriteLine();
             Console.ReadKey();
 
+            value = 30.685m * (decimal)Math.Pow(10, 9);
+            digits = 4;
+
+            Console.WriteLine("Rounding away from zero:");
+            for (int i = 0; i < 10; i++)
+            {
+                decimal roundedValue = MathExt.RoundSignificant(value + i * (decimal)Math.Pow(10, 9), digits);
+                Console.WriteLine("input value: {0} - Rounded value: {1}", ((double)value + i * Math.Pow(10, 9)).ToString(), roundedValue.ToString());
+                //value++;
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        public void FactorialDemo()
+        {
+            Console.WriteLine("Factorial as double:");
             for (double i = 0; i < 180; i++)
             {
                 Console.WriteLine(i.ToString() + " " + MathExt.Factorial(i).ToString());
             }
+            Console.WriteLine();
             Console.ReadKey();
 
+            Console.WriteLine("Factorial as decimal:");
             for (int i = 0; i < 30; i++)
             {
                 Console.WriteLine(i.ToString() + " " + MathExt.Factorial(i).ToString());
             }
-            decimal f = MathExt.Factorial(20);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        public void FibonacciDemo()
+        {
+            Console.WriteLine("Fibonacci as double:");
+            for (int i = 0; i < 1500; i++)
+            {
+                Console.WriteLine(i.ToString() + " " + MathExt.Fibonacci((double)i).ToString());
+            }
+            Console.WriteLine();
             Console.ReadKey();
 
-            //for (int i = 0; i < 1500; i++)
-            //{
-            //    Console.WriteLine(i.ToString() + " " + MathExt.Fibonacci((double)i).ToString());
-            //}
-            //Console.ReadKey();
-
-            //for (int i = 0; i < 150; i++)
-            //{
-            //    Console.WriteLine(i.ToString() + " " + MathExt.Fibonacci(i).ToString());
-            //}
-            //Console.ReadKey();
-            decimal dest = MathExt.RoundSignificant(decimal.MaxValue, 27, false, MidpointRounding.AwayFromZero);
-            Console.WriteLine(decimal.MaxValue.ToString());
-            Console.WriteLine(dest.ToString());
-            Console.ReadKey();
-
-            //double test = MathExt.RoundSignificant(123456789012345678d, 10, false, MidpointRounding.AwayFromZero);
-            //Console.WriteLine(test.ToString());
-            //Console.ReadKey();
-
-            decimal d = 120000000000.345m;
-            Console.WriteLine(MathExt.RoundUp(d, 28, UpRounding.AwayFromZero).ToString());
-            Console.WriteLine(MathExt.RoundUp(d, 28, UpRounding.Up).ToString());
-            Console.WriteLine(MathExt.RoundUp(-d, 28, UpRounding.AwayFromZero).ToString());
-            Console.WriteLine(MathExt.RoundUp(-d, 28, UpRounding.Up).ToString());
-            Console.WriteLine(MathExt.RoundUp(d, -1, UpRounding.AwayFromZero).ToString());
-            Console.WriteLine(MathExt.RoundUp(d, -1, UpRounding.Up).ToString());
-            Console.WriteLine(MathExt.RoundUp(-d, -1, UpRounding.AwayFromZero).ToString());
-            Console.WriteLine(MathExt.RoundUp(-d, -1, UpRounding.Up).ToString());
+            Console.WriteLine("Fibonacci as decimal:");
+            for (int i = 0; i < 150; i++)
+            {
+                Console.WriteLine(i.ToString() + " " + MathExt.Fibonacci(i).ToString());
+            }
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
-
 
 }
